148d7dac02249f8af2c4b6859da2ccb4f995c07d
Tomás Cordisco <58890694+tomcordisco@users.noreply.github.com>
2021-09-22 22:29:10 -0300

    Update Pages.js

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Pages = props => {
     return (
-        <div className="row justify-content-center">
-            <button onClick={props.pageBack} type="button" className="btn btn-primary btn-lg">&larr; Back</button>
-            <button onClick={props.pageNext} type="button" className="btn btn-primary btn-lg">Next &rarr;</button>
+        <div className="row justify-content-center ">
+            <button onClick={props.pageBack} type="button" className="btn btn-primary btn-lg mb-3">&larr; Back</button>
+            <button onClick={props.pageNext} type="button" className="btn btn-primary btn-lg mb-3">Next &rarr;</button>
         </div>
     )
 }
